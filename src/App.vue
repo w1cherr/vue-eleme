b14a5075ff5c2814f587e004298481c661637b8b
xiong <wakexbear@gmail.com>
2017-08-24 15:23:07 +0800

    axios调用json数据

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,69 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <div class="header">
+      <v-header :seller="seller"></v-header>
+    </div>
+    <div class="tab board-1px">
+      <div class="tab-item">
+        <router-link to="/goods" tag="a">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app'
-}
+<script type="text/ecmascript-6">
+  import header from './components/header/header.vue'
+
+  const ERR_OK = 0
+
+  export default {
+    data() {
+      return {
+        seller: {}
+      }
+    },
+    created() {
+      console.log(this.$axios)
+      this.$axios.get('/api/seller?id=')
+      .then((response) => {
+        response = response.data
+        if (response.errno === ERR_OK) {
+          this.seller = Object.assign({}, this.seller, response.data)
+        }
+      })
+    },
+    components: {
+      'v-header': header
+    }
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
+  .tab
+    display flex
+    width 100%
+    height 40px
+    line-height 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    // border-bottom 1px solid rgba(7, 17, 27, 0.1)
+    .tab-item
+      flex 1
+      text-align center
+      & > a
+        display block
+        font-size 14px
+        color rgb(77, 85, 93)
+      .router-link-active
+        color: rgb(240, 20, 20)
 </style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    i am retings
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'retings'
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
