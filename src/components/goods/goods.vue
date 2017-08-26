17b4670ee321882e541c6e292daa1b052f8ff8dd
xiong <wakexbear@gmail.com>
2017-08-26 11:51:38 +0800

    商品栏左侧

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,14 +1,92 @@
 <template>
   <div>
-    i am goods
+    <div class="goods">
+      <div class="menu-wrapper">
+        <ul>
+          <li v-for="(item,index) in goods" class="menu-item">
+            <span class="text border-1px">
+              <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div class="foods-wrapper"></div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  const ERR_OK = 0
   export default {
-    name: 'goods'
+    data() {
+      return {
+        goods: []
+      }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+      this.$axios.get('/api/goods').then((response) => {
+        response = response.data
+        if (response.errno === ERR_OK) {
+          this.goods = response.data
+        }
+      })
+    }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
+  .goods
+    display flex
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    overflow hidden
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background #f3f5f7
+      .menu-item
+        display table
+        height 54px
+        width 56px
+        padding 0 12px
+        line-height 14px
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background: #fff
+          font-weight: 700
+          .text
+            border-none()
+        .icon
+          display inline-block
+          vertical-align top
+          width 12px
+          height 12px
+          margin-right 2px
+          background-size 12px 12px
+          background-repeat no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          border-1px(rgba(7, 17, 27, 0.1))
+          font-size: 12px
+    .foods-wrapper
+      flex 1
 </style>
